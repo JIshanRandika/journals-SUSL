2c1925e59ec78542fdf7e24c40ba08057a75e0af
JIshanRandika <jishanrandika@gmail.com>
2022-12-29 07:23:55 +0530

    update navigation

--- src/components/nav.component.tsx
+++ src/components/nav.component.tsx
@@ -18,7 +18,7 @@ export default function NavComponent() {
                 {['sm'].map((expand) => (
                     <Navbar  bg="light" expand={expand} >
                         <Container fluid>
-                            <Navbar.Brand href="#">SUSL - Journals</Navbar.Brand>
+                            <Navbar.Brand href="#">LOGOS</Navbar.Brand>
                             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
                             <Navbar.Offcanvas
                                 id={`offcanvasNavbar-expand-${expand}`}
@@ -33,12 +33,21 @@ export default function NavComponent() {
                                 <Offcanvas.Body>
                                     <Nav className="justify-content-end flex-grow-1 pe-3">
                                         <Nav.Link href="#action1">Home</Nav.Link>
-                                        <Nav.Link href="#action2">About</Nav.Link>
-                                        <Nav.Link href="#action2">Current</Nav.Link>
-                                        <Nav.Link href="#action2">Archives</Nav.Link>
+                                        {/*<Nav.Link href="#action2">Current</Nav.Link>*/}
+                                        <NavDropdown title="Editorial Board" id="navbarScrollingDropdown">
+                                            <NavDropdown.Item href="#action3">Board of Editors</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action4">
+                                                Editorial Advisory Board
+                                            </NavDropdown.Item>
+                                            <NavDropdown.Divider />
+                                            <NavDropdown.Item href="#action5">
+                                                Journal Structure
+                                            </NavDropdown.Item>
+                                        </NavDropdown>
                                         <Nav.Link href="#action2">Submission</Nav.Link>
                                         <Nav.Link href="#action2">Announcement</Nav.Link>
                                         <Nav.Link href="#action2">Contact</Nav.Link>
+
                                         {/*<NavDropdown*/}
                                         {/*    title="Dropdown"*/}
                                         {/*    id={`offcanvasNavbarDropdown-expand-${expand}`}*/}
