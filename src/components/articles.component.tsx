b9c72f81e8cfd267166f0a7b673f9cfbd61f1bf0
JIshanRandika <jishanrandika@gmail.com>
2023-05-16 22:34:17 +0530

    update

--- src/components/articles.component.tsx
+++ src/components/articles.component.tsx
@@ -30,11 +30,28 @@ export default class ArticlesComponent extends Component {
                     paddingTop: 5,
                     paddingBottom: 0,
                     color: 'black',
+                    fontWeight:"bold",
+                    // marginTop:10
+                    // fontFamily:'Itim',
+                }}>
+                    <a style={{textDecoration:'none'}} href='assets/articles/EditorialNote.pdf' target='_blank'>
+                        Editorial Note
+                    </a>
+                </div>
+
+                <hr data-aos="fade-up" data-aos-offset="100" />
+                <div data-aos="fade-up" data-aos-offset="100" style={{
+                    fontSize: 15,
+                    textAlign: 'left',
+                    wordSpacing: 3,
+                    paddingTop: 5,
+                    paddingBottom: 0,
+                    color: 'black',
                     fontWeight:"bold"
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-CHN-13.pdf' target='_blank'>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-CHN-13.pdf' target='_blank'>
                         谈国别化旅游汉语教材编写中的实用性问题——以《斯里兰卡旅游汉语》 为例
                     </a>
                 </div>
@@ -49,7 +66,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    孙 瑛，S.Nimali.Priyangika 重庆师范大学 文学院，重庆 401331；2四川大学 文学与新闻学院，成都 610065、斯里 兰卡佛教与巴利大学 语言学院 10206
+                    孙 瑛，S. Nimali Priyangika
                 </div>
 
 
@@ -65,7 +82,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-CHN-17.pdf' target='_blank'>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-CHN-17.pdf' target='_blank'>
                         泰戈尔《飞鸟集》对冰心小诗的影响
                     </a>
                 </div>
@@ -80,7 +97,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    朱松 黄冈师范学院，外国语学院，英语语言文学系
+                    朱松
                 </div>
 
 
@@ -96,7 +113,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-ESL-05.pdf' target='_blank'>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-ESL-05.pdf' target='_blank'>
                         Effectiveness of Current Curriculum in Enhancing Speaking Ability: A Study Based on Technical Colleges
                     </a>
                 </div>
@@ -111,7 +128,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    P.N. Gardihewa English Language Teaching Center, Gampaha Wickramarachchi University of Indigenous Medicine, Sri Lanka
+                    P.N. Gardihewa
                 </div>
 
                 <hr data-aos="fade-up" data-aos-offset="100" />
@@ -126,7 +143,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-ESL-14.pdf' target='_blank'>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-ESL-14.pdf' target='_blank'>
                         An Exploratory Analysis of Non-starter Undergraduates’ Learning Needs for a Scaffolding ESP Curriculum
                     </a>
                 </div>
@@ -141,7 +158,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    W.M.P.Y.B. Rathnayake Department of Languages, University of Moratuwa, Sri Lanka
+                    W.M.P.Y.B. Rathnayake
                 </div>
 
 
@@ -158,8 +175,9 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-HND-7.pdf' target='_blank'>
-                        laL—fr vkSj vFkZfoKku ds ifjisk esa 'kCnksa dk vFkZ&ifjorZu% fganh ds laL—r rRle 'kCnksa ds vk/kkj ij                                </a>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-HND-7.pdf' target='_blank'>
+                        संस्कृति और अर्थविज्ञान के परिपेक्ष में शब्दों का अर्थ-परिवर्तनः हिंदी के संस्कृत तत्सम शब्दों के आधार पर
+                    </a>
                 </div>
                 <div data-aos="fade-up" data-aos-offset="100" style={{
                     fontSize: 15,
@@ -172,7 +190,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    H. I. Prematilake Department of Hindi Studies, University of Kelaniya, Kelaniya, Sri Lanka.
+                    H.I. Prematilake
                 </div>
 
 
@@ -188,7 +206,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-JPN-15.pdf' target='_blank'>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-JPN-15.pdf' target='_blank'>
                         謝罪場面での「事情説明」における自動詞・他動詞の使用状況 ―スリランカの日本語学習者を対象に―
                     </a>
                 </div>
@@ -203,7 +221,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    D.P.O.Dassanayaka Division of Japanese Language and Culture, Graduate School of Language and Culture, Osaka University of Japan
+                    D.P.O. Dassanayaka
                 </div>
 
                 <hr data-aos="fade-up" data-aos-offset="100" />
@@ -218,7 +236,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-JPN-18.pdf' target='_blank'>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-JPN-18.pdf' target='_blank'>
                         シンハラ語と日本語の対照研究 ― 文学翻訳における言語構造分析を通して ―
                     </a>
                 </div>
@@ -233,7 +251,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    Y Nishina J E Mohotti Graduate School of Humanities and Social Sciences, Hiroshima University, Japan Department of Languages, Sabaragamuwa University of Sri Lanka
+                    Y. Nishina, J.E. Mohotti
                 </div>
 
                 <hr data-aos="fade-up" data-aos-offset="100" />
@@ -248,8 +266,8 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-SNH-4.pdf' target='_blank'>
-                        ÈUq,d., fjfik jeÈ ckhdf.a ck weoys,s iy úYajdij, woH;k iajrEmh mssn úu¾Ykd;aul wOHhkhla ( ÈUq,d., m%foaYSh f,alï fldÜGdifha myhlal=f¾" o¿ldk iy ñ,a,dk .ïudk weiqfrks'
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-SNH-4.pdf' target='_blank'>
+                        දිඹුලාගල වෙසෙන වැදි ජනයාගේ ජන ඇදහිලි සහ විශ්වාසවල අද්‍යතන ස්වරූපය පිළිබඳ විමර්ශනාත්මක අධ්‍යයනයක් : දිඹුලාගල ප්‍රදේශීය ලේකම් කොට්ඨාසයේ පහළ යක්කුරේ, දළුකාන සහ මිල්ලාන ගම්මාන ඇසුරෙනි.
                     </a>
                 </div>
                 <div data-aos="fade-up" data-aos-offset="100" style={{
@@ -263,7 +281,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    1wd¾ ta ã m%shxld ùrfialr 2tÉ tï nqoaêl fyar;a 12NdId wOHkdxYh" Y%S ,xld inr.uqj úYajúoHd,h
+                    ආර්.ඒ.ඩී. ප්‍රියංකා වීරසේකර, එච්.එම්. බුද්ධික හේරත්
                 </div>
 
 
@@ -279,8 +297,9 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-SNH-9.pdf' target='_blank'>
-                        wfmalaIdNx.;ajh" fÄokSh iudc m%;súmdl flfrys n,mdk wdldrh wekd lerkskd kjl;dj weiqßka flfrk wOHhkhls                                </a>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-SNH-9.pdf' target='_blank'>
+                        අපේක්ෂාභංගත්වය, ඛේදනීය සමාජ ප්‍රතිවිපාක කෙරෙහි බලපාන ආකාරය ඇනා කැරනිනා නවකතාව ඇසුරින් කෙරෙන අධ්‍යයනයකි
+                    </a>
                 </div>
                 <div data-aos="fade-up" data-aos-offset="100" style={{
                     fontSize: 15,
@@ -293,9 +312,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    î¡ù¡whs¡tiA O¾ufiak1" puSmd tia¡o is,ajd2
-                    NdId wOHhkdxYh1"2
-                    › ,xld inr.uqj úYajúoHd,h
+                    බී.වී.අයි.එස්. ධර්මසේන, චම්පා එස්.ද සිල්වා
                 </div>
 
 
@@ -311,7 +328,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-TML-11.pdf' target='_blank'>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-TML-11.pdf' target='_blank'>
                         உரைநரை வளர்ச்சியில் ஆர்zல்ட் சதாசிவம் பிள்ரள
                     </a>
                 </div>
@@ -326,7 +343,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    S.R Devakumari Department of Languages, Sabaragamuwa University of Sri Lanka
+                    S.R. Devakumari
                 </div>
 
                 <hr data-aos="fade-up" data-aos-offset="100" />
@@ -341,8 +358,9 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    <a href='assets/articles/2022-AA-TRL-8.pdf' target='_blank'>
-                        Study on the Effectiveness of Using Google Translate in Legal Translation: With Special Reference to Selected Legal Documents of the Registrar General’s Department                                </a>
+                    <a style={{textDecoration:'none'}} href='assets/articles/2022-AA-TRL-8.pdf' target='_blank'>
+                        Study on the Effectiveness of Using Google Translate in Legal Translation: With Special Reference to Selected Legal Documents of the Registrar General’s Department
+                    </a>
                 </div>
                 <div data-aos="fade-up" data-aos-offset="100" style={{
                     fontSize: 15,
@@ -355,7 +373,7 @@ export default class ArticlesComponent extends Component {
                     // marginTop:10
                     // fontFamily:'Itim',
                 }}>
-                    Ubhayawardhana P.D.N.M.1, Hansani J.A.M., Department of Languages, Sabaragamuwa University of Sri Lanka, Belihuloya, Sri Lanka
+                    P.D.N.M. Ubhayawardhana, J.A.M. Hansani
                 </div>
 
 
